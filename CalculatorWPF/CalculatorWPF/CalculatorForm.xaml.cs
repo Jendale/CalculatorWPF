e0803f6e408cee242b4361def7dfa6ba60032228
Jendale Kim Ramay <ramayjendale@gmail.com>
2020-03-31 20:09:44 +0800

    Updated some button functionality.

--- CalculatorWPF/CalculatorWPF/CalculatorForm.xaml.cs
+++ CalculatorWPF/CalculatorWPF/CalculatorForm.xaml.cs
@@ -179,6 +179,7 @@ namespace CalculatorWPF
 
             num1 = num2 = 0;
             operation = "";
+            isPeriodUsed = false;
         }
 
         private void BtnDelete_Click(object sender, RoutedEventArgs e)
